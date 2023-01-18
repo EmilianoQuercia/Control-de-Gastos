d83d5b10f7697fd772710d3979b337409019c2ba
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-01-18 13:43:01 -0300

    Primer commit, ya esta armado la pantalla de nuevo presupuesto, control de presupuesto y nuevo gasto

---
+++ src/components/Modal.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import CerrarBtn from '../img/cerrar.svg'
+
+
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
+
+    const ocultarModal = () =>{
+        setAnimarModal(false)
+        
+        setTimeout(()=>{
+            setModal(false)
+        },500)
+    }
+
+    return (
+        <div className='modal'>
+            <div className='cerrar-modal'>
+                <img src={CerrarBtn} alt="cerrar modal"
+                    onClick={ocultarModal} />
+            </div>
+            
+            <form className={`formulario ${animarModal ? 'animar':'cerrar'}`}>
+                <legend>Nuevo Gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre del Gasto</label>
+
+                    <input type="text" id="nombre" placeholder='Añade el nombre del gasto' />
+                </div>
+                
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+
+                    <input type="number" id="cantidad" placeholder='Añade la cantidad del gasto ej: 300' />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Categoría</label>
+
+                    <select id='categoria'>
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="subscripciones">Subscripciones</option>
+                    </select>
+                </div>
+                <input type="submit" value={'Añadir Gasto'} />
+            </form>
+        </div>
+    );
+}
+
+export default Modal;
