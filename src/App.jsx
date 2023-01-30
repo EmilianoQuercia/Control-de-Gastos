8767b9a733c34e70f0f3cb21366bc5d820031ef5
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-01-30 20:25:54 -0300

    Agregue funcion eliminar y filtrar por gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Filtros from './components/Filtros';
 import Header from './components/Header'
 import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
@@ -9,12 +10,18 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   
-  const [gastos, setGastos] = useState([]);
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  );
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
   const [gastoEditar, setGastoEditar] = useState({});
+  const [filtro, setFiltro] = useState('');
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
 
   useEffect(() => {
     if(Object.keys(gastoEditar).length > 0) {
@@ -25,6 +32,29 @@ function App() {
       },500)
     }
   }, [gastoEditar]);
+
+  useEffect(()=>{
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  },[presupuesto])
+
+  useEffect(()=>{
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? []);
+  },[gastos])
+
+  useEffect(()=>{
+    if(filtro){
+      //filtrar gastos por categoria
+      const gastosFiltrados = gastos.filter(gasto => gasto.categoria === filtro); 
+      setGastosFiltrados(gastosFiltrados)
+    }
+  },[filtro])
+
+  useEffect(()=>{
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+  },[])
   
   const handleNuevoGasto = () => {
     setModal(true);
@@ -70,10 +100,16 @@ function App() {
       {isValidPresupuesto && (
         <>
         <main>
+          <Filtros
+            filtro={filtro}
+            setFiltro={setFiltro}
+          />
           <ListadoGastos
             gastos={gastos}
             setGastoEditar = {setGastoEditar}
             eliminarGasto = {eliminarGasto}
+            filtro={filtro}
+            gastosFiltrados={gastosFiltrados}
           />   
         </main>
         <div className='nuevo-gasto'>
--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import {CircularProgressbar} from 'react-circular-progressbar'
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css';
+
+
 const ControlPresupuesto = ({gastos, presupuesto}) => {
 
+    const[porcentaje, setPorcentaje] = useState(0)
     const[disponible, setDisponible]=useState(0)
     const[gastado, setGastado]=useState(0)
 
     useEffect(()=>{
         const totalGastado = gastos.reduce((total, gasto)=> gasto.cantidad + total, 0)
         const totalDisponible = presupuesto - totalGastado
-
+        
+        //calcular el porcentaje gastado
+        const nuevoPorcentaje = (((presupuesto - totalDisponible)/ presupuesto)*100).toFixed(2);
         setDisponible(totalDisponible)
         setGastado(totalGastado)
+        setTimeout(()=>{
+            setPorcentaje(nuevoPorcentaje); 
+        }, 1500)
+
     },[gastos])
+
 
     //funcion para cambiar el numero del presupuesto a formato dinero
     const formatearCantidad = (cantidad) => {
@@ -26,14 +36,20 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
         <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
             <div>
                 <CircularProgressbar
-                    value={0}
+                styles={buildStyles({
+                    pathColor: porcentaje > 100 ? 'red': '#3b82f6',
+                    trailColor: '#f5f5f5',
+                    textColor: '#3b82f6',
+                })}
+                    value={porcentaje}
+                    text={`${porcentaje}% Gastado`}
                 />
             </div>
             <div className='contenido-presupuesto'>
                 <p>
                     <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
                 </p>
-                <p>
+                <p className={`${disponible < 0 ? 'negativo' : ''}`}>
                     <span>Disponible:</span> {formatearCantidad(disponible)}
                 </p>
                 <p>
